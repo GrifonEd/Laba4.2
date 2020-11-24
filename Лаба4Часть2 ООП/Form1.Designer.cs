e5672763c2a07a6c457a35b2e0186667c993032a
Edik Daukaev <daukaev.eduard@mail.ru>
2020-11-24 11:17:39 +0500

    Первое изменение

--- Лаба4Часть2 ООП/Form1.Designer.cs
+++ Лаба4Часть2 ООП/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 412);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(776, 26);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(371, 269);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(417, 137);
+            this.progressBar1.TabIndex = 1;
+            this.progressBar1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.progressBar1_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(353, 137);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Увеличить размер";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Лаба4Часть2 ООП/Form1.cs
+++ Лаба4Часть2 ООП/Form1.cs
@@ -16,5 +16,44 @@ namespace Лаба4Часть2_ООП
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Size = new System.Drawing.Size( this.button1.Width+10, this.button1.Height + 10);
+            if ((progressBar1.Value)<= 90)
+            progressBar1.Value +=10;
+            textBox1.Text = button1.Width.ToString();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                 
+                button1.Size = new System.Drawing.Size(Int32.Parse(textBox1.Text), Int32.Parse(textBox1.Text));
+                if (Int32.Parse(textBox1.Text) > 100)
+                    progressBar1.Value = 100;
+                else if (Int32.Parse(textBox1.Text) < 0)
+                    progressBar1.Value = 0;
+                else
+                    progressBar1.Value = Int32.Parse(textBox1.Text);
+            }
+        }
+
+        private void progressBar1_MouseMove(object sender, MouseEventArgs e)
+        {
+            textBox1.Text = e.X.ToString();
+            button1.Size = new System.Drawing.Size(e.X, e.X);
+        }
     }
+
+
+  /*  public class Model
+    {
+        private int value;
+        public void setValue
+        {
+
+        }
+    }*/
 }
